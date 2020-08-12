71787ee552a2a971eef6455d355d52c13b9c7b95
Mehmet Gani Tombalak <mgani.tombalak@yahoo.com>
2020-08-12 14:41:16 +0300

    React Day-I State,Props,Event Handling,Conditional Rendering

--- React/hunter-tarnet/src/components/card/index.js
+++ React/hunter-tarnet/src/components/card/index.js
@@ -1,18 +1,46 @@
 import React from 'react'
-import faker from 'faker';
+import Approval from './components/approval'
+// CLASS COMPONENT
+class Card extends React.Component {
+    state = { isApproved: false };
+    constructor(props) {
+        super(props);
+        // this.onApprove=this.onApprove.bind(this);
+        // this.onReject=this.onReject.bind(this);
+    }
+    onApprove = () => {
+        //console.log(this); UNDEFINED
+        // this.state.isApproved=true; WRONG
+        this.setState({ isApproved: true });
+        console.log("Approved");
+    }
 
-const Card = () => <div className="ui card">
-    <div className="image">
-        <img src={faker.image.avatar()} />
-    </div>
-    <div className="content">
-        <a className="header">{faker.name.firstName()}</a>
-        <div className="meta">
-            <span className="date">Joined in {faker.date.past().getFullYear()}</span>
+    onReject = () => {
+        this.setState({ isApproved: false });
+        console.log("Rejected");
+    }
+
+    render() {
+        const approval =
+            this.state.isApproved ?
+                null :
+                <Approval onApprove={this.onApprove} onReject={this.onReject} />
+        return <div className="ui card">
+            <div className="image">
+                <img src={this.props.avatar} />
+            </div>
+            <div className="content">
+                <a className="header">{this.props.firstName}</a>
+                <div className="meta">
+                    <span className="date">Joined in {this.props.joinDate}</span>
+                </div>
+                <div className="description">{this.props.jobTitle}</div>
+            </div>
+            <div className="extra content"><a><i className="user icon"></i>{this.props.friendCount} Friends</a></div>
+            {approval}
         </div>
-        <div className="description">{faker.name.jobTitle()}</div>
-    </div>
-    <div className="extra content"><a><i className="user icon"></i>{faker.random.number(500)} Friends</a></div>
-</div>
+    }
+
+}
 
 export default Card;
