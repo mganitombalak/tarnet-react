a69ed17d2f779cea1a819c68ca9f930b940ad12f
Mehmet Gani Tombalak <mgani.tombalak@yahoo.com>
2020-08-13 15:58:17 +0300

    MapStateToProps

--- React/hunter-tarnet/src/pages/category/list/index.js
+++ React/hunter-tarnet/src/pages/category/list/index.js
@@ -9,7 +9,10 @@ export class CategoryList extends React.Component {
         axios.get('http://178.128.248.160:81/api/category', {
             headers: { Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImRkNmNhY2IzLTMzNTQtNDUwMC04ODEyLWYwYjQ4NWY4YjRlNCIsImVtYWlsIjoibWdhbmkudG9tYmFsYWtAeWFob28uY29tIiwicm9sZSI6Im51bGwiLCJuYmYiOjE1OTczMjMyMTMsImV4cCI6MTU5NzMyNDExMywiaWF0IjoxNTk3MzIzMjEzfQ.-BqG2GYova6Ejuj7BjY7Clb7_WN6_GwisGACsFQqawo' }
         })
-            .then(response => this.props.dispatch(setDataActionCreator(ENTITY_TYPE_CATEGORY,response.data.data)))
+            .then(response => {
+                // this.props.dispatch(setDataActionCreator(ENTITY_TYPE_CATEGORY,response.data.data))
+                this.props.setDataActionCreator(ENTITY_TYPE_CATEGORY,response.data.data);
+            })
             .catch(err => console.log(err));
         //throw("My Exception"); //Testing for ErrorCatcher
         const result = this.props.kategoriler && this.props.kategoriler.length > 0 ?
@@ -21,4 +24,5 @@ export class CategoryList extends React.Component {
 
 const mapStateToProps = state => { return { kategoriler: state?.categories ?? [] } }
 
-export default connect(mapStateToProps)(CategoryList)
+// export default connect(mapStateToProps)(CategoryList)
+export default connect(mapStateToProps,{setDataActionCreator})(CategoryList)
