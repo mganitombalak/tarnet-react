3fd8421f0cebecc6dda7aeffef8f524e21b0af1d
okanbilke <okanbilke@gmail.com>
2020-08-13 10:58:08 +0300

    Datatables

--- React/hunter-tarnet/src/pages/category/list/index.js
+++ React/hunter-tarnet/src/pages/category/list/index.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import axios from 'axios';
+import Datatable from './DataTable'
 
-export default class CategoryList extends React.Component {
+class CategoryList extends React.Component 
+{
     state = { data: [] }
-    render() {
-        axios.get('http://178.128.248.160:81/api/category', {
-            headers: { Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImRkNmNhY2IzLTMzNTQtNDUwMC04ODEyLWYwYjQ4NWY4YjRlNCIsImVtYWlsIjoibWdhbmkudG9tYmFsYWtAeWFob28uY29tIiwicm9sZSI6Im51bGwiLCJuYmYiOjE1OTczMDI1MzcsImV4cCI6MTU5NzMwMzQzNywiaWF0IjoxNTk3MzAyNTM3fQ.3nVtDPWoQ3BQ18VJ7vT7TRg5ens8dXk64JExqPYjgpE' }
-        })
+    render() 
+    {
+        axios.get('http://178.128.248.160:81/api/category',
+            {
+                headers: { Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImRkNmNhY2IzLTMzNTQtNDUwMC04ODEyLWYwYjQ4NWY4YjRlNCIsImVtYWlsIjoibWdhbmkudG9tYmFsYWtAeWFob28uY29tIiwicm9sZSI6Im51bGwiLCJuYmYiOjE1OTczMDQ1NTAsImV4cCI6MTU5NzMwNTQ1MCwiaWF0IjoxNTk3MzA0NTUwfQ.fomWU0La-lbq5lJXiCJ937-cTOZAN2tzJR2OSEwqG_Q' }
+            })
             .then(response => this.setState({ data: response.data.data }))
-            .catch (err => console.log(err));
+            .catch(err => console.log(err));
         return <table className="ui selectable celled table">
             <thead>
                 <tr>
                     <th>#</th>
                     <th>Name</th>
                     <th>Status</th>
-                    <th>Createad At</th>
+                    <th>Created At</th>
                     <th>Action</th>
                 </tr>
             </thead>
-            <tbody>
-                {this.state.data.map((item,index) => <tr key={index}><td>{item.displayOrder}</td><td>{item.name}</td><td>{item.isActive}</td><td>{item.createdAt}</td><td><i className="trash alternate icon"></i> <i className="edit icon"></i></td></tr>)}
-            </tbody>
+                <Datatable data={this.state.data}></Datatable>
         </table>
     }
 }
+
+export default CategoryList;
