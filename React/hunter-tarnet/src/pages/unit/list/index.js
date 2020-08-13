06ef7b87316949c1daedcd3fd8c5e2d55d67cb22
ttufekci <ttufekci@tarimkredi.org>
2020-08-13 16:26:21 +0300

    unit

--- React/hunter-tarnet/src/pages/unit/list/index.js
+++ React/hunter-tarnet/src/pages/unit/list/index.js
@@ -2,27 +2,28 @@ import React from 'react';
 import axios from 'axios';
 import DataTable from '../../../components/data-table';
 import { connect } from 'react-redux';
-import { setDataActionCreator, ENTITY_TYPE_CATEGORY } from '../../../actions';
-export class CategoryList extends React.Component {
+import { setDataActionCreator, ENTITY_TYPE_UNIT } from '../../../actions';
+
+export class UnitList extends React.Component {
     // state = { data: [] }
     render() {
-        axios.get('http://178.128.248.160:81/api/category', {
+        axios.get('http://178.128.248.160:81/api/unit', {
             headers: { Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImRkNmNhY2IzLTMzNTQtNDUwMC04ODEyLWYwYjQ4NWY4YjRlNCIsImVtYWlsIjoibWdhbmkudG9tYmFsYWtAeWFob28uY29tIiwicm9sZSI6Im51bGwiLCJuYmYiOjE1OTczMjQ4NDQsImV4cCI6MTU5NzMyNTc0NCwiaWF0IjoxNTk3MzI0ODQ0fQ.k8ZWsBDKWG5ekqYjmMHGpztTLuHehLzWjgFIKN00Xdo' }
         })
             .then(response => {
                 // this.props.dispatch(setDataActionCreator(ENTITY_TYPE_CATEGORY,response.data.data))
-                this.props.setDataActionCreator(ENTITY_TYPE_CATEGORY,response.data.data);
+                this.props.setDataActionCreator(ENTITY_TYPE_UNIT,response.data.data);
             })
             .catch(err => console.log(err));
         //throw("My Exception"); //Testing for ErrorCatcher
-        const result = this.props.kategoriler && this.props.kategoriler.length > 0 ?
-            <DataTable data={this.props.kategoriler} /> :
+        const result = this.props.birimler && this.props.birimler.length > 0 ?
+            <DataTable data={this.props.birimler} /> :
             <div>No data</div>;
         return <div className="ui container">{result}</div>
     }
 }
 
-const mapStateToProps = state => { return { kategoriler: state?.categories ?? [] } }
+const mapStateToProps = state => { return { birimler: state?.units ?? [] } }
 
 // export default connect(mapStateToProps)(CategoryList)
-export default connect(mapStateToProps,{setDataActionCreator})(CategoryList)
+export default connect(mapStateToProps,{setDataActionCreator})(UnitList)
