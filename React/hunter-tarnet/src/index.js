917426efa52990e98e03a905ea5a28925fdafe33
Mehmet Gani Tombalak <mgani.tombalak@yahoo.com>
2020-08-13 11:44:15 +0300

    Exception Handling

--- React/hunter-tarnet/src/index.js
+++ React/hunter-tarnet/src/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import Root from './components/root';
 import {BrowserRouter} from 'react-router-dom';//HashRouter,MemoryRouter
-
-ReactDOM.render(<BrowserRouter><Root></Root></BrowserRouter>, document.querySelector('#root'));
+import ErrorCatcher from './components/error-boundry';
+ReactDOM.render(<ErrorCatcher><BrowserRouter><Root></Root></BrowserRouter></ErrorCatcher>, document.querySelector('#root'));
--- React/hunter-tarnet/src/pages/category/list/index.js
+++ React/hunter-tarnet/src/pages/category/list/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import axios from 'axios';
 import DataTable from '../../../components/data-table';
-import ErrorCatcher from '../../../components/error-boundry';
 export default class CategoryList extends React.Component {
     state = { data: [] }
     render() {
+        //throw("My Exception"); //Testing for ErrorCatcher
         const result = this.state.data.length > 0 ? <DataTable data={this.state.data} /> : null;
         axios.get('http://178.128.248.160:81/api/category', {
             headers: { Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImRkNmNhY2IzLTMzNTQtNDUwMC04ODEyLWYwYjQ4NWY4YjRlNCIsImVtYWlsIjoibWdhbmkudG9tYmFsYWtAeWFob28uY29tIiwicm9sZSI6Im51bGwiLCJuYmYiOjE1OTczMDY4NzgsImV4cCI6MTU5NzMwNzc3OCwiaWF0IjoxNTk3MzA2ODc4fQ.VXBOac6K45-Ozh1cK3aAsdy6YI4aqTQSNg1QJW8R3jc' }
         })
             .then(response => this.setState({ data: response.data.data }))
             .catch(err => console.log(err));
-        return <ErrorCatcher>{result}</ErrorCatcher>
+        return <div className="ui container">{result}</div>
     }
 }
