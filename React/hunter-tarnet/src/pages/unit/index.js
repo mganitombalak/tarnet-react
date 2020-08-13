7d778196bcaa79331d4ae084ccc52144dd32020e
orkun yasar <orkunyasarrr@hotmail.com>
2020-08-13 16:12:16 +0300

    redux

--- React/hunter-tarnet/src/pages/unit/index.js
+++ React/hunter-tarnet/src/pages/unit/index.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import axios from 'axios';
+import { connect } from 'react-redux';
+import { setDataActionCreator, ENTITY_TYPE_UNIT } from '../../actions';
+import DataTable from '../../components/data-table';
 
-const Unit =()=> <div>Unit page!</div>
+class Unit extends React.Component{
 
-export default Unit;
+    render() {
+        axios.get('http://178.128.248.160:81/api/unit', {
+            headers: { Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImRkNmNhY2IzLTMzNTQtNDUwMC04ODEyLWYwYjQ4NWY4YjRlNCIsImVtYWlsIjoibWdhbmkudG9tYmFsYWtAeWFob28uY29tIiwicm9sZSI6Im51bGwiLCJuYmYiOjE1OTczMjQyMjEsImV4cCI6MTU5NzMyNTEyMSwiaWF0IjoxNTk3MzI0MjIxfQ.VVkxPM99eGT1grH7XHt4J8pHv7OO0Xlxo-Abaxbqn0U' }
+        })
+            .then(response => this.props.dispatch(setDataActionCreator(ENTITY_TYPE_UNIT,response.data.data)))
+            .catch(err => console.log(err));
+        //throw("My Exception"); //Testing for ErrorCatcher
+        const result = this.props.units && this.props.units.length > 0 ?
+            <DataTable data={this.props.units} /> :
+            <div>No data</div>;
+        return <div className="ui container">{result}</div>
+    }
+
+}
+
+const mapStateToProps = state => { return { units: state?.units ?? [] } }
+
+export default connect(mapStateToProps)(Unit)
--- React/hunter-tarnet/src/reducers/index.js
+++ React/hunter-tarnet/src/reducers/index.js
@@ -1,19 +1,26 @@
-import { ACTION_SET_DATA, ENTITY_TYPE_CATEGORY } from "../actions";
+import {
+  ACTION_SET_DATA,
+  ENTITY_TYPE_CATEGORY,
+  ENTITY_TYPE_UNIT,
+} from "../actions";
 
 const initials = {
-    categories: []
+  categories: [],
+  units: [],
 };
 
 export default (state = initials, action) => {
-    // console.log(state);
-    // console.log(action);
-    // console.log("Store has been created");
-    if (action.type === ACTION_SET_DATA) {
-        switch (action.entityType) {
-            case ENTITY_TYPE_CATEGORY:
-                return { categories: action.payload };
-            default:
-                return state;
-        }
+  // console.log(state);
+  // console.log(action);
+  // console.log("Store has been created");
+  if (action.type === ACTION_SET_DATA) {
+    switch (action.entityType) {
+      case ENTITY_TYPE_CATEGORY:
+        return { categories: action.payload };
+      case ENTITY_TYPE_UNIT:
+        return { units: action.payload };
+      default:
+        return state;
     }
-}
+  }
+};
